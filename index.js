2b101eb6ead26601cd62cfb04a3fffe3dafad8ae
Mickael <mickael.spilmont.fr@gmail.com>
2019-08-19 13:00:15 +0200

    index.js get form data

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hello !!!");
+
+document.getElementById("form").addEventListener("submit", event => {
+    event.preventDefault();
+
+    const firstCity = document.getElementById("firstCity").value;
+    const secondCity = document.getElementById("secondCity").value;
+
+    console.log(firstCity);
+    console.log(secondCity);
+
+    const method = document.getElementsByName("method");
+
+    if (method[0].checked) {
+        console.log("Promise");
+    } else {
+        console.log("Async/Await");
+    }
+})
